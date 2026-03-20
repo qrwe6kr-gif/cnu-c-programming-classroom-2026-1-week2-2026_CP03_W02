d73997e85b5029554d2e7fd8d4f2e4002399cdb4
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 09:55:22 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+int count = 0;
 
-int main()
-{
+int power(int base, int exp) {
+    base * base;
+    count++;
+    power(b)
+        if (count==)
 
+}
+
+int main() {
+    printf("%d\n", power(2, 3));
+    printf("%d\n", power(5, 0));
+    printf("%d\n", power(3, 4));
 
     return 0;
 }
