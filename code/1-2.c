7f8829d17f498c410d73ef1a12fb8d1ba78e1ad7
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 09:45:38 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-int main()
-{
-
+void print() {
+    printf("welcome to computer Programming 03 Lab!\n");
+}
+int square(int n) {
+    return n * n;
+}
+int main() {
+    print();
+    
+    int num = 5;
+    int result = square(num);
+    printf("%d %d \n", num, result);
 
     return 0;
 }
