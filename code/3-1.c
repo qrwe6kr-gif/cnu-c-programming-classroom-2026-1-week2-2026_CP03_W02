dbbb4a407dbe97b4f40d95c035682bd2db2077b8
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 10:18:37 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+int count=0;
 
-int main()
-{
+int call_count() {
+    static int count = 0;
+    
+    return ++count;
+}
 
+int main() {
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    
     return 0;
 }
 
