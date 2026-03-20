487826fb76d794dd35c8bf394bc58f7a8fe4a723
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 10:22:57 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
+int global_var = 0;
+
+void test_scope() {
+    static int static_var = 0;
+    int local_var = 0;
+
+    global_var++;
+    static_var++;
+    local_var++;
+    printf("global_var: %d\n", global_var);
+    printf("static_var: %d\n", static_var);
+    printf("local_var: %d\n", local_var);
+}
+
+int main() {
+    test_scope();
+    printf("\n");
+    test_scope();
+    printf("\n");
+    test_scope():
 
     return 0;
 }
