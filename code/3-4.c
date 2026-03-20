869672877bbf6a962cf8d00a2a586ead7fd3c1f7
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 10:28:03 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
+void max_of() {
+    return 
+    
+}
 
+int main() {
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+    
     return 0;
 }
 
