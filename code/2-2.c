10f83b1667e4c06cf00a21ba661368c3ef59940b
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 10:11:10 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+void swap(int *x, int *y) {
+    
+   *x = addr. Of x;
+   *y = addr.Of y;
+    x = *y;
+    y = *x;
+}
 
+int main() {
+    int x= 100; 
+    int y = 200;
+
+    printf("x = %d, y = %d\n", x, y);
+
+    swap( );
+    printf("x = %d, y = %d\n", x, y);
+    
     return 0;
 }
 
